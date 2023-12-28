308cae05e0cbc6f219dfd646048bc4634d448313
jcarstenson <j.carstenson@gmail.com>
2023-12-28 14:51:52 -0600

    ingredient picker: add header section with title and clear ingredients button

--- src/components/IngredientPicker.tsx
+++ src/components/IngredientPicker.tsx
@@ -1,6 +1,7 @@
 import { AppContext } from "../App";
 import { INGREDIENTS } from "../services/IngredientService";
 import { useContext } from "react";
+import styled from "styled-components";
 
 const IngredientPicker = () => {
   const appContext = useContext(AppContext);
@@ -20,8 +21,52 @@ const IngredientPicker = () => {
     appContext.setToggledIngredients(() => newToggledIngredients);
   };
 
+  const clear = () => {
+    if (appContext === null) {
+      return;
+    }
+
+    const newToggledIngredients = appContext.toggledIngredients.map(
+      () => false
+    );
+
+    appContext.setToggledIngredients(() => newToggledIngredients);
+  };
+
+  const Header = styled.div`
+    display: flex;
+    padding: 0 0 0.5em 0;
+  `;
+
+  const Title = styled.h3`
+    margin-bottom: 0;
+  `;
+
+  const ClearButton = styled.button`
+    align-self: end;
+    margin-left: auto;
+    text-transform: uppercase;
+    border: none;
+    background-color: inherit;
+
+    :hover {
+      background-color: #d9d9d9;
+      border-radius: 5px;
+    }
+  `;
+
+  const HeaderLine = styled.hr`
+    border: 1px solid black;
+    margin-top: 0;
+  `;
+
   return (
     <div>
+      <Header>
+        <Title>Ingredients</Title>
+        <ClearButton onClick={() => clear()}>Clear</ClearButton>
+      </Header>
+      <HeaderLine />
       {INGREDIENTS.map((ingredient) => (
         <button
           key={ingredient.name}
